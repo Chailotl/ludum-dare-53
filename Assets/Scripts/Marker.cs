ed7661e443ad848b1d1de4294bc816b6b487274f
Chai <7232280+Chailotl@users.noreply.github.com>
2023-04-29 19:56:11 -0400

    Fixed markers

--- Assets/Scripts/Marker.cs
+++ Assets/Scripts/Marker.cs
@@ -1,19 +1,73 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Marker : MonoBehaviour
 {
 	[SerializeField]
 	private Transform target;
 	[SerializeField]
-	private float width = 50f;
+	private float border = 50f;
+    [SerializeField]
+    private Sprite boxSprite;
+    [SerializeField]
+    private Sprite arrowSprite;
+
+    private Image image;
+
+    void Start()
+	{
+        image = GetComponent<Image>();
+	}
 
 	void Update()
 	{
-		Vector3 pos = Camera.main.WorldToScreenPoint(target.position);
-		pos.x = Mathf.Clamp(pos.x, width, Camera.main.pixelWidth - width);
-		pos.y = Mathf.Clamp(pos.y, width, Camera.main.pixelHeight - width);
-		transform.position = pos;
-	}
+        Vector3 screenPos = Camera.main.WorldToScreenPoint(target.position);
+        bool isTargetVisible = screenPos.z > 0 && screenPos.x > 0 && screenPos.y > 0 && screenPos.x < Screen.width && screenPos.y < Screen.height;
+        Vector3 screenCenter = new Vector3(Screen.width, Screen.height, 0) / 2;
+        Vector3 screenBounds = new Vector3(screenCenter.x - border, screenCenter.y - border, 0);
+
+        if (isTargetVisible)
+        {
+            transform.position = screenPos;
+            transform.rotation = Quaternion.identity;
+
+            image.sprite = boxSprite;
+        }
+        else
+        {
+            screenPos -= screenCenter;
+
+            if (screenPos.z < 0) { screenPos *= -1; }
+
+            float angle = Mathf.Atan2(screenPos.y, screenPos.x);
+            float slope = Mathf.Tan(angle);
+
+            if (screenPos.x > 0)
+            {
+                screenPos = new Vector3(screenBounds.x, screenBounds.x * slope, 0);
+            }
+            else
+            {
+                screenPos = new Vector3(-screenBounds.x, -screenBounds.x * slope, 0);
+            }
+
+            if (screenPos.y > screenBounds.y)
+            {
+                screenPos = new Vector3(screenBounds.y / slope, screenBounds.y, 0);
+            }
+            else if (screenPos.y < -screenBounds.y)
+            {
+                screenPos = new Vector3(-screenBounds.y / slope, -screenBounds.y, 0);
+            }
+
+            screenPos += screenCenter;
+
+            transform.position = screenPos;
+            transform.rotation = Quaternion.Euler(0, 0, angle * Mathf.Rad2Deg);
+
+            image.sprite = arrowSprite;
+        }
+    }
 }
